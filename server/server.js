32f64ebb2f530888352a83a1338093e5b382e67e
SAINIKHIL-27 <your-github-email@example.com>
2026-05-06 20:41:29 +0530

    FIX: Force lowercase filename for user model

--- server/server.js
+++ server/server.js
@@ -21,4 +21,4 @@ app.use('/api/dashboard', require('./routes/dashboardRoutes'));
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
