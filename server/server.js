e6d44ca0e125ff5d1ca45818b5adf314d9f041a5
SAINIKHIL-27 <your-github-email@example.com>
2026-05-07 00:21:40 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || '*' }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
